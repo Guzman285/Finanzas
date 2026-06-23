765b62f57c245fc91852baa6752fe83136147227
guzman <herberthguzman0@gmail.com>
2026-06-23 12:54:40 -0600

    index

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../includes/app.php';
 use MVC\Router;
 use Controllers\AppController;
 use Controllers\CuentaController;
+use Controllers\BancoController;
 
 $router = new Router();
 $router->setBaseURL('/' . $_ENV['APP_NAME']);
@@ -17,6 +18,9 @@ $router->get('/API/cuentas/buscar',     [CuentaController::class, 'buscarAPI']);
 $router->post('/API/cuentas/guardar',   [CuentaController::class, 'guardarAPI']);
 $router->post('/API/cuentas/modificar', [CuentaController::class, 'modificarAPI']);
 $router->post('/API/cuentas/eliminar',  [CuentaController::class, 'eliminarAPI']);
+
+// Bancos
+$router->get('/API/bancos/buscar', [BancoController::class, 'buscarAPI']);
 
 // Comprueba y valida las rutas
 $router->comprobarRutas();
