a56185ca3062426774131a68fccc8b31d69ce1c2
guzman <herberthguzman0@gmail.com>
2026-06-23 13:20:00 -0600

    web

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,38 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    'js/app' : './src/js/app.js',
-    'js/inicio' : './src/js/inicio.js',
-    'js/cuentas/index': './src/js/cuentas/index.js',
+    "js/app": "./src/js/app.js",
+    "js/inicio": "./src/js/inicio.js",
+    "js/cuentas/index": "./src/js/cuentas/index.js",
+    "js/bancos/index": "./src/js/bancos/index.js",
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'public/build')
+    filename: "[name].js",
+    path: path.resolve(__dirname, "public/build"),
   },
   plugins: [
     new MiniCssExtractPlugin({
-        filename: 'styles.css'
-    })
+      filename: "styles.css",
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.(c|sc|sa)ss$/,
         use: [
-            {
-                loader: MiniCssExtractPlugin.loader
-            },
-            'css-loader',
-            'sass-loader'
-        ]
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.(png|svg|jpe?g|gif)$/,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
-    ]
-  }
+    ],
+  },
 };
